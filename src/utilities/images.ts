fa260a5e8737b077581f02069dfa006db81d03e7
George Fairbairn <george.fair@icloud.com>
2024-11-17 13:59:49 +0000

    feat: update photos in polaroid strip

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -1,8 +1,20 @@
-import gAndI from '../assets/g_and_i.jpeg';
-import hideThePain from '../assets/hide_the_pain.jpeg';
+import arches from '../assets/arches.jpg';
+import cave from '../assets/cave.jpg';
+import japan from '../assets/japan.jpg';
+import olympics from '../assets/olympics.jpg';
+import oslo from '../assets/oslo.jpg';
+import panorama from '../assets/panorama.jpg';
+import plane from '../assets/plane.jpg';
+import surfing from '../assets/surfing.jpg';
 import type { ImageMetadata } from 'astro';
 
 export const IMAGES: Record<string, ImageMetadata> = {
-  glastonbury: gAndI,
-  harold: hideThePain
+  arches,
+  cave,
+  japan,
+  olympics,
+  oslo,
+  panorama,
+  plane,
+  surfing
 } as const;
